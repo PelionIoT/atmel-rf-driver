9d4361e9c56a70b477807c6e10b7257043bdf667
Seppo Takalo <seppo.takalo@arm.com>
2014-12-08 20:47:44 +0200

    AT24MAC I2C drivers.

---
+++ source/at24mac.cpp
@@ -0,0 +1,57 @@
+#include "at24mac.h"
+#include "mbed.h"
+
+/* Device addressing */
+#define AT24MAC_EEPROM_ADDRESS		(0x0A<<4)
+#define AT24MAC_RW_PROTECT_ADDRESS	(0x06<<4)
+#define AT24MAC_SERIAL_ADDRESS		(0x0B<<4)
+
+/* Known memory blocks */
+#define AT24MAC_SERIAL_OFFSET	(0x80)
+#define AT24MAC_EUI64_OFFSET	(0x98)
+#define AT24MAC_EUI48_OFFSET	(0x98)
+
+#define SERIAL_LEN 16
+#define EUI64_LEN 8
+#define EUI48_LEN 6
+
+I2C i2c(D14, D15);
+
+/**
+ * Read unique serial number from chip.
+ * \param buf pointer to write serial number to. Must have space for 16 bytes.
+ * \return zero on success, negative number on failure
+ */
+extern "C" int at24mac_read_serial(void *buf)
+{
+	char offset = AT24MAC_SERIAL_OFFSET;
+	if (!i2c.write(AT24MAC_SERIAL_ADDRESS, &offset, 1, true))
+		return -1; //No ACK
+	return i2c.read(AT24MAC_SERIAL_ADDRESS, (char*)buf, SERIAL_LEN);
+}
+
+/**
+ * Read EUI-64 from chip.
+ * \param buf pointer to write EUI-64 to. Must have space for 8 bytes.
+ * \return zero on success, negative number on failure
+ */
+extern "C" int at24mac_read_eui64(void *buf)
+{
+	char offset = AT24MAC_EUI64_OFFSET;
+	if (!i2c.write(AT24MAC_SERIAL_ADDRESS, &offset, 1, true))
+		return -1; //No ACK
+	return i2c.read(AT24MAC_SERIAL_ADDRESS, (char*)buf, EUI64_LEN);
+}
+
+/**
+ * Read EUI-48 from chip.
+ * \param buf pointer to write EUI-48 to. Must have space for 6 bytes.
+ * \return zero on success, negative number on failure
+ */
+extern "C" int at24mac_read_eui48(void *buf)
+{
+	char offset = AT24MAC_EUI48_OFFSET;
+	if (!i2c.write(AT24MAC_SERIAL_ADDRESS, &offset, 1, true))
+		return -1; //No ACK
+	return i2c.read(AT24MAC_SERIAL_ADDRESS, (char*)buf, EUI48_LEN);
+}
---
+++ source/at24mac.h
@@ -0,0 +1,44 @@
+#ifndef AT2MAC_H
+#define AT24MAC_H
+
+/*
+ * AT24MAC drivers.
+ *
+ * This is a EEPROM chip designed to contain factory programmed read-only EUI-64 or EUI-48,
+ * a 128bit serial number and some user probrammable EEPROM.
+ *
+ * AT24MAC602 contains EUI-64, use at24mac_read_eui64()
+ * AT24MAC402 contains EUI-64, use at24mac_read_eui48()
+ *
+ * NOTE: You cannot use both EUI-64 and EUI-48. Chip contains only one of those.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Read uniqua serial number from chip.
+ * \param buf pointer to write serial number to. Must have space for 16 bytes.
+ * \return zero on success, negative number on failure
+ */
+int at24mac_read_serial(void *buf);
+
+/**
+ * Read EUI-64 from chip.
+ * \param buf pointer to write EUI-64 to. Must have space for 8 bytes.
+ * \return zero on success, negative number on failure
+ */
+int at24mac_read_eui64(void *buf);
+/**
+ * Read EUI-48 from chip.
+ * \param buf pointer to write EUI-48 to. Must have space for 6 bytes.
+ * \return zero on success, negative number on failure
+ */
+int at24mac_read_eui48(void *buf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AT24MAC_H */
