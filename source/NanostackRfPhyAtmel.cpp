72027c09520d83e8529dc5f62204a5a596ff1ece
Kari Severinkangas <kari.severinkangas@arm.com>
2018-12-13 16:16:27 +0200

    Handling of tx/rx cleaned-up. (#76)
    * Handling of tx/rx cleaned-up.

--- source/NanostackRfPhyAtmel.cpp
+++ source/NanostackRfPhyAtmel.cpp
@@ -1078,21 +1078,20 @@ static void rf_if_interrupt_handler(void)
 
     /*Frame end interrupt (RX and TX)*/
     if (irq_status & TRX_END) {
-        /*TX done interrupt*/
         rf_trx_states_t trx_status = rf_if_trx_status_from_full(full_trx_status);
-        if (trx_status == PLL_ON || trx_status == TX_ARET_ON) {
+        if (rf_flags & RFF_TX) {
             rf_handle_tx_end(trx_status);
-        }
-        /*Frame received interrupt*/
-        else {
+        } else if (rf_flags & RFF_RX) {
             rf_handle_rx_end(trx_status);
+        } else {
+            //something went really wrong
         }
     }
     if (irq_status & CCA_ED_DONE) {
         rf_handle_cca_ed_done(full_trx_status);
     }
     if (irq_status & TRX_UR) {
-        // Here some counter could be used to monitor the underrun occurancy count.
+        // Here some counter could be used to monitor the under run occurrence count.
         // Do not print anything here!
     }
 }
